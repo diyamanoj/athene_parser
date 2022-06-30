7393d79090b343d6fef4da4513b8ab87e48f5557
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2022-06-30 11:34:35 +0530

    athene_parser: dup and scrape * Scrape the line before getTransport * Check and remove duplicate entry

--- parse_log.py
+++ parse_log.py
@@ -3,6 +3,12 @@ output = open("output.txt", "w")
 
 search_term = "getTransport: Cannot find entry"
 
+reviewed_lines = set()
+
+# Scrape and check for duplicate entry
 for line in log_file:
     if search_term in line:
-        output.write(line)
+        line_splitted = line[line.index("getTransport"):]
+        if line_splitted not in reviewed_lines:
+            reviewed_lines.add(line_splitted)
+            output.write(line_splitted)
