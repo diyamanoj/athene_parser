ec3a7082350d6d0167beb3364a50fdd60d6a4b65
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2022-06-30 11:23:46 +0530

    getTransport parser

--- parse_log.py
+++ parse_log.py
@@ -1,1 +1,8 @@
-print("Hello")
+log_file = open("log.txt", "r")
+output = open("output.txt", "w")
+
+search_term = "getTransport: Cannot find entry"
+
+for line in log_file:
+    if search_term in line:
+        output.write(line)
